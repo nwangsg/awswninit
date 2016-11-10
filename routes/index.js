9f90418aa530bae026c91e22cf924c9f85002b20
wangning47 <nwangsg@gmail.com>
2016-11-11 00:04:24 +0800

    import material and bootstrap

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var router = express.Router();
 var path = require('path');
+var viewpath = path.join(__dirname,'/../views')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  	res.sendFile(path.join(__dirname,'/../views/index.html'));
+  	res.sendFile(path.join(viewpath,'/index.html'));
 });
 
 module.exports = router;
