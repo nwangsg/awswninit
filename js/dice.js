c46cea4895395b9dc4a9ca30c1d2ac3f56256020
wangning47 <nwangsg@gmail.com>
2016-11-15 23:00:24 +0800

    remove unused js file, include babylonjs

--- js/dice.js
+++
@@ -1,3 +0,0 @@
-function redirectToIndex(){
-	window.location = "/";
-}
--- js/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-function redirectToDice() {
-    window.location.href = '/dice';
-}
-exports.redirectToDice = redirectToDice;
